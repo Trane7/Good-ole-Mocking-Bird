a71eded5987969fe0e2e2aa5781c336a6d57c5c0
Dee <dgrowette@gmail.com>
2021-10-21 14:22:38 -0700

    added prompt for number of characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,33 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+
+}
+
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt("How long do you want your password, between 8-128 characters");
+  if (passwordLength !== null) {
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Invaild input, please input between 8 and 128")
+  }  
+} 
+  const includesLowerCase = confirm("Do you want to include any Lowercase letters?")
+  const includesUpperCase = confirm("Do you want to include any Uppercase letters?")
+  const includesNumbers = confirm("Do you want to include any Numbers?")
+  const includesSpecial = confirm("Do you want to include any Special Characters?")
+  const hasSelectorType = includesLowerCase || includesUpperCase || includesNumbers || includesSpecial;
+  if (hasSelectorType) {
+    
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+  passwordText.value = password; 
 
 }
 
