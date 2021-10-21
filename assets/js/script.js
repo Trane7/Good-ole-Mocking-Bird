4d11ea6558ae6ac76dafb0e0ae7ee5ca19959563
Dee <dgrowette@gmail.com>
2021-10-21 14:27:40 -0700

    added alert to inform one must be selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,14 +17,21 @@ function writePassword() {
     passwordLength = prompt("Invaild input, please input between 8 and 128")
   }  
 } 
-  const includesLowerCase = confirm("Do you want to include any Lowercase letters?")
-  const includesUpperCase = confirm("Do you want to include any Uppercase letters?")
-  const includesNumbers = confirm("Do you want to include any Numbers?")
-  const includesSpecial = confirm("Do you want to include any Special Characters?")
-  const hasSelectorType = includesLowerCase || includesUpperCase || includesNumbers || includesSpecial;
-  if (hasSelectorType) {
-    
+  let includesLowerCase = confirm("Do you want to include any Lowercase letters?")
+  let includesUpperCase = confirm("Do you want to include any Uppercase letters?")
+  let includesNumbers = confirm("Do you want to include any Numbers?")
+  let includesSpecial = confirm("Do you want to include any Special Characters?")
+  let hasSelectorType = includesLowerCase || includesUpperCase || includesNumbers || includesSpecial;
+  while (!hasSelectorType) {
+    alert("Atleast one character type should be selected!")
+     includesLowerCase = confirm("Do you want to include any Lowercase letters?")
+     includesUpperCase = confirm("Do you want to include any Uppercase letters?")
+     includesNumbers = confirm("Do you want to include any Numbers?")
+     includesSpecial = confirm("Do you want to include any Special Characters?")
+     hasSelectorType = includesLowerCase || includesUpperCase || includesNumbers || includesSpecial;
   }
+    
+  
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
